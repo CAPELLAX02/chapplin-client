42f48297824b2ac3b0c03405e6990c9760569142
CAPELLAX02 <ahmtatar@gmail.com>
2024-08-23 02:56:18 +0300

    Create User

--- src/components/auth/Signup.tsx
+++ src/components/auth/Signup.tsx
@@ -1,10 +1,25 @@
 import { Link } from 'react-router-dom';
 import { Link as MUILink } from '@mui/material';
 import Auth from './Auth';
+import { useCreateUser } from '../../hooks/useCreateUser';
 
 const Signup = () => {
+  const [createUser] = useCreateUser();
+
   return (
-    <Auth submitLabel="Sign Up" onSubmit={async () => {}}>
+    <Auth
+      submitLabel="Sign Up"
+      onSubmit={async ({ email, password }) => {
+        await createUser({
+          variables: {
+            createUserInput: {
+              email,
+              password,
+            },
+          },
+        });
+      }}
+    >
       <Link to={'/login'} style={{ alignSelf: 'center' }}>
         <MUILink>Login</MUILink>
       </Link>
