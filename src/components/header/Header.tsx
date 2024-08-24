a714377c3bf003d0eb7c7321f58ab809567fa22f
CAPELLAX02 <ahmtatar@gmail.com>
2024-08-24 19:19:55 +0300

    Header

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,27 @@
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Container from '@mui/material/Container';
+import Branding from './Branding';
+import MobileNavigation from './mobile/MobileNavigation';
+import MobileBranding from './mobile/MobileBrending';
+import Navigation from './Navigation';
+import Settings from './Settings';
+
+const pages: string[] = [];
+
+const Header = () => {
+  return (
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Branding />
+          <MobileNavigation pages={pages} />
+          <MobileBranding />
+          <Navigation pages={pages} />
+          <Settings />
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+};
+export default Header;
