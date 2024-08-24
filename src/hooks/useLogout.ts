ae4368c389a536d1f0609bbeb7d60941b94aed57
CAPELLAX02 <ahmtatar@gmail.com>
2024-08-24 19:56:42 +0300

    Error Snack Messages - UX

--- src/hooks/useLogout.ts
+++ src/hooks/useLogout.ts
@@ -2,9 +2,13 @@ import { API_URL } from '../constants/urls';
 
 const useLogout = () => {
   const logout = async () => {
-    await fetch(`${API_URL}/auth/logout`, {
+    const res = await fetch(`${API_URL}/auth/logout`, {
       method: 'POSt',
     });
+
+    if (!res.ok) {
+      throw new Error('Error logging out.');
+    }
   };
 
   return { logout };
