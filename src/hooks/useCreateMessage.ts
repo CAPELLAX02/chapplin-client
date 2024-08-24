3aab7fb6c6ae2ebed7f17dd3b7cf3372d2f4dc2e
CAPELLAX02 <ahmtatar@gmail.com>
2024-08-25 01:42:26 +0300

    Update Messages Cache

--- src/hooks/useCreateMessage.ts
+++ src/hooks/useCreateMessage.ts
@@ -1,5 +1,7 @@
 import { useMutation } from '@apollo/client';
 import { graphql } from '../gql';
+import { MessageFragment } from '../fragments/message.fragment';
+import { getMessagesDocument } from './useGetMessages';
 
 const createMessageDocument = graphql(`
   mutation CreateMessage($createMessageInput: CreateMessageInput!) {
@@ -9,8 +11,31 @@ const createMessageDocument = graphql(`
   }
 `);
 
-const useCreateMessage = () => {
-  return useMutation(createMessageDocument);
+const useCreateMessage = (chatId: string) => {
+  return useMutation(createMessageDocument, {
+    update(cache, { data }) {
+      const messagesQueryOptions = {
+        query: getMessagesDocument,
+        variables: {
+          chatId,
+        },
+      };
+      const messages = cache.readQuery({
+        ...messagesQueryOptions,
+      });
+
+      if (!messages || !data?.createMessage) {
+        return;
+      }
+
+      cache.writeQuery({
+        ...messagesQueryOptions,
+        data: {
+          messages: messages.messages.concat(data?.createMessage),
+        },
+      });
+    },
+  });
 };
 
 export { useCreateMessage };
